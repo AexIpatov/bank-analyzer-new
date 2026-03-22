688f992b4493a3f774edcaf18d32a14d81e078da
AexIpatov <ipatov.findir@gmail.com>
2026-03-22 13:38:07 +0300

    Update koruna_unicredit.py

--- parsers/koruna_unicredit.py
+++ parsers/koruna_unicredit.py
@@ -1,5 +1,5 @@
 from .garpiz_unicredit import GarpizUnicreditParser
 
 class KorunaUnicreditParser(GarpizUnicreditParser):
-    """Парсер для Koruna UniCredit- CZK (Excel/CSV)"""
+    """Парсер для Koruna UniCredit- CZK_0226.csv"""
     pass
