2c3ee770a7e4256266028cf5fa93547eab33cd2f
AexIpatov <ipatov.findir@gmail.com>
2026-03-22 12:50:43 +0300

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,3 +1,33 @@
-from .simple_parser import SimpleParser
+from .antonijas_industra import AntonijasIndustraParser
+from .antonijas_revolut import AntonijasRevolutParser
+from .industra_plavas import IndustraPlavasParser
+from .paysera_property import PayseraPropertyParser
+from .paysera_rerum import PayseraRerumParser
+from .paysera_sveciy import PayseraSveciyParser
+from .twohills_molly import TwoHillsMollyParser
+from .wio_business import WioBusinessParser
+from .mashreq_nomiqa import MashreqNomiqaParser
+from .pasha_bunda import PashaBundaParser
+from .dzibik_csob import DzibikCsobParser
+from .garpiz_unicredit import GarpizUnicreditParser
+from .koruna_unicredit import KorunaUnicreditParser
+from .bunda_aed import BundaAEDParser
+from .bunda_azn import BundaAZNParser
 
-__all__ = ['SimpleParser']
+__all__ = [
+    'AntonijasIndustraParser',
+    'AntonijasRevolutParser',
+    'IndustraPlavasParser',
+    'PayseraPropertyParser',
+    'PayseraRerumParser',
+    'PayseraSveciyParser',
+    'TwoHillsMollyParser',
+    'WioBusinessParser',
+    'MashreqNomiqaParser',
+    'PashaBundaParser',
+    'DzibikCsobParser',
+    'GarpizUnicreditParser',
+    'KorunaUnicreditParser',
+    'BundaAEDParser',
+    'BundaAZNParser',
+]
