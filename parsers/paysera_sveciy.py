66109358015867ab1c63583ef3fa05f660aba6d8
AexIpatov <ipatov.findir@gmail.com>
2026-03-22 13:21:48 +0300

    Create paysera_sveciy.py

---
+++ parsers/paysera_sveciy.py
@@ -0,0 +1,5 @@
+from .paysera_property import PayseraPropertyParser
+
+class PayseraSveciyParser(PayseraPropertyParser):
+    """Парсер для Paysera Sveciy Namai Lithuania EUR (Excel/CSV)"""
+    pass
